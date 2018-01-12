6170c5baf61499467a1673952163d353735fe439
Greg Lawrence <gregaudio@gmail.com>
2018-01-12 14:29:34 -0600

    refactored app.Config2

---
+++ app/app.Config2.js
@@ -0,0 +1,9 @@
+angular.module("BikeLogApp").constant("FIREBASE_CONFIG", {
+    
+    apiKey: FBK,
+    authDomain: "bike-component-log.firebaseapp.com",
+    databaseURL: "https://bike-component-log.firebaseio.com",
+    projectId: "bike-component-log",
+    storageBucket: "bike-component-log.appspot.com",
+    messagingSenderId: "801372508673"
+})

3d2a29d92d7a41dcf46cb891c4cde01c06a1b9cd
Greg Lawrence <gregaudio@gmail.com>
2018-01-21 00:31:04 -0600

    followed tutorial to deploy firebase app, issues with a few files on load such as NavCtrl, BikeFactory

--- app/app.Config2.js
+++
@@ -1,9 +0,0 @@
-angular.module("BikeLogApp").constant("FIREBASE_CONFIG", {
-    
-    apiKey: "{{$FBK.value}}",
-    authDomain: "bike-component-log.firebaseapp.com",
-    databaseURL: "https://bike-component-log.firebaseio.com",
-    projectId: "bike-component-log",
-    storageBucket: "bike-component-log.appspot.com",
-    messagingSenderId: "801372508673"
-})
